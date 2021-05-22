081fa36cedc5ba0dd746a4127019cb18b062a2a8
Virginijus03 <klimauskas@yahoo.com>
2021-05-22 10:49:24 +0300

    validation

---
+++ Validation.js/index.js
@@ -0,0 +1,22 @@
+const Validation = require('./Validation');
+
+
+/*class Validation {
+    isValid () {}
+    method2 () {}
+    method3 () {}
+}
+*/
+
+const v = new Validation();
+
+console.log(v.isValidFirstName('Vardenis'), true);
+console.log(v.isValidLastName('Pavardenis'), true);
+console.log(v.isValidEmail('demo@example.com'), true);
+console.log(v.isValidMessage('Some message'), true);
+console.log(v.isValidPhoneNumber(+37061234567), true);
+console.log(v.isValidMonthName('Sausis'), true);
+console.log(v.isValidMonthName('January'), true);
+console.log(v.isValidWeekdayName('Pirmadienis'), true);
+console.log(v.isValidWeekdayName('Monday'), true);
+console.log(v.isValidPersonID(39912311234), true);
